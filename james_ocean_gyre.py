46b40415ab6c47aaf0f9fca59cbae53f6d5f8062
James Hammond <james.hammond@merton.ox.ac.uk>
2020-11-30 16:14:57 +0000

    Added plotting functionality, updated scheme

--- james_ocean_gyre.py
+++ james_ocean_gyre.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 # define the simulation constants
 r = 1.0 
@@ -12,20 +14,34 @@ ymax = 1 # setting this to 1, as per the suggestion that r/beta*ymax = 1
 # and now the simulation parameters
 xsteps = 100
 ysteps = xsteps
+xstep = (xmax - xmin) / xsteps
+ystep = (ymax - ymin) / ysteps
 # define the array in which we store the psi(x,y) values
 simulation = np.zeros((xsteps, ysteps), dtype=float)
-# define a series of functions which compute the coefficients in the problem matrix
-def first_coeff(lambd, xstep, ystep):
-    return -lambd * xtsep**2
-def second_coeff(lambd, xstep, ystep):
-    return (xstep - lambd) * ystep**2 + 2 * lambd * xstep**2
-def third_coeff(lambd, xstep, ystep):
-    return -lambd * xtsep**2
-# and likewise for the right hand side (rhs)
-def first_rhs(lambd, xstep, ystep):
-    return (2*lambd - xstep) * ystep**2
-def second_rhs(lambd, xstep, ystep):
-    return (2*lambd - xstep) * ystep**2
+# define a function which computes the coefficients on the LHS of the scheme
+def left_coeff(lambd, xstep, ystep):
+    return 1 - lambd / xstep
+l1 = left_coeff(lambd, xsteps, ysteps)
+# now solve for each value 
+for x in range(1, xsteps-1):
+    for y in range(2, ysteps-1):
+        # calculate terms in the scheme
+        frac1 = (-2 * simulation[x, y] + simulation[x-1, y]) / xstep**2
+        frac2 = (simulation[x, y+1] - 2 * simulation[x, y] + simulation[x, y-1]) / ystep**2 
+        # now calculate the new value
+        new_val = simulation[x, y] + xstep * lambd * (frac1 + frac2)
+        new_value = new_val / l1
+        # now append 
+        simulation[x+1, y] = new_value
+
+print(simulation)
+# now visualise
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+X = np.linspace(xmin, xmax, xsteps)
+Y = np.linspace(ymin, ymax, ysteps)
+ax.plot_surface(X=X, Y=Y, Z=simulation)
+plt.savefig("test.png")
 
 
 
